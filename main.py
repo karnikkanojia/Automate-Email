dd6d0023049276eb0bfc5e5375630b16920e14dc
karnikkanojia <karnikk1406120@gmail.com>
2021-10-25 19:26:19 +0530

    Send email code added

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-from decouple import config
-import requests
-
-from bs4 import BeautifulSoup
-
+import datetime
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import datetime
+import requests
+from bs4 import BeautifulSoup
+from decouple import config
 
 now = datetime.datetime.now()
 
-content = ''
 
 def extract_news(url):
-    print('Extracting Hacker News Stories....')
-    body=''
-    body+=('<b>HN Top Stories:<b>\n'+'<br>'+'-'*50+'<br>')
-    response=requests.get(url)
-    content=response.content
-    soup=BeautifulSoup(content,'html.parser')
-    for i,tag in enumerate(soup.find_all('td', attrs={'class':'title','valign':''})):
-        body+=((str(i+1)+' :: '+tag.text+'\n'+'<br>') if tag.text!='More' else '')
+    print('Extracting Hacker News Stories...')
+    body = ''
+    body += ('<b>HN Top Stories:<b>\n' + '<br>' + '-' * 50 + '<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i, tag in enumerate(
+            soup.find_all('td', attrs={
+                'class': 'title',
+                'valign': ''
+            })):
+        body += ((str(i + 1) + ' :: ' + tag.text + '\n' +
+                  '<br>') if tag.text != 'More' else '')
     return body
+
+content = extract_news('https://news.ycombinator.com/')
+content+=('<br>--------<br>')
+content+=('<br><br>End of Message')
+
+print('Composing Email')
+
+SERVER='smtp.gmail.com'
+PORT=587
+FROM=config('EMAIL')
+TO=config('EMAIL')
+PASSWORD=config('PASSWORD')
+
+msg = MIMEMultipart()
+
+msg['Subject']='Top New Stories HN [Automated Email]'+' '+str(now.day)+'-'+str(now.month)+'-'+str(now.year)
+msg['From']=FROM
+msg['To']=TO
+
+msg.attach(MIMEText(content,'html'))
+
+print('Initiating Server...')
+
+server=smtplib.SMTP(SERVER,PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM,PASSWORD)
+server.sendmail(FROM,TO,msg.as_string())
+
+print('Email sent...')
+
+server.quit()
