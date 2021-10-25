65a4d8de087712cf73061f0561f7c64b55cae448
Karnik Kanojia <karnikkanojia@Karniks-MacBook-Pro.local>
2021-10-25 18:54:33 +0530

    Code Initialized

---
+++ main.py
@@ -0,0 +1,14 @@
+from decouple import config
+import requests
+
+from bs4 import BeautifulSoup
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import datetime
+
+now = datetime.datetime.now()
+
+content = ''
