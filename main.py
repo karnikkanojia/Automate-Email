6f3a96db58c3df7333d0c87055f3f498543178c5
karnikkanojia <karnikk1406120@gmail.com>
2021-10-25 19:06:49 +0530

    Extracting news routine Added
    Made .env file accordingly

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ import datetime
 now = datetime.datetime.now()
 
 content = ''
+
+def extract_news(url):
+    print('Extracting Hacker News Stories....')
+    body=''
+    body+=('<b>HN Top Stories:<b>\n'+'<br>'+'-'*50+'<br>')
+    response=requests.get(url)
+    content=response.content
+    soup=BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td', attrs={'class':'title','valign':''})):
+        body+=((str(i+1)+' :: '+tag.text+'\n'+'<br>') if tag.text!='More' else '')
+    return body
